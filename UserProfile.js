1c39e1639f85d8dcfcba978053e028a1a1161694
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-10-30 20:37:00 +0800

    UserProfile.js

---
+++ UserProfile.js
@@ -0,0 +1,12 @@
+// collect the following user information:
+let FullName = "Ashley Valdez"
+let Age = "19"
+let FavoriteNumber = "11"
+let FavoriteColor = "light green"
+
+// use a variable to store this information and log it to the console
+
+console.log("FullName:",FullName);
+console.log("Age:",Age);
+console.log("FavoriteNumber:",FavoriteNumber);
+console.log("FavoriteColor:",FavoriteColor);
